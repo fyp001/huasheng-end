55a1e8cc6e1327fe1a96d49445b6bb13b972270a
GeneLi <1227021319@qq.com>
2020-12-29 16:24:11 +0800

    拦截ljh

--- src/main/java/com/myproject/api/springboot_mybatis/config/InterceptorConfig.java
+++ src/main/java/com/myproject/api/springboot_mybatis/config/InterceptorConfig.java
@@ -16,8 +16,8 @@ public class InterceptorConfig implements WebMvcConfigurer {
     @Bean
     public LoginInterceptor getMyInterceptor(){return new LoginInterceptor();}
 
-    @Value("${spring.mvc.static-path-pattern}")
-    private String staticPathPattern;
+//    @Value("${spring.mvc.static-path-pattern}")
+//    private String staticPathPattern;
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         // 注册拦截器
@@ -39,7 +39,7 @@ public class InterceptorConfig implements WebMvcConfigurer {
         //ir.addPathPatterns("staff/*");
         // 添加不拦截的请求
         //ir.excludePathPatterns("/staff/login");
-        ir.excludePathPatterns(this.staticPathPattern);
+        ir.excludePathPatterns("/static/**");
         ir.excludePathPatterns("/static/assets/**");
         ir.excludePathPatterns("/static/img/**");
         ir.excludePathPatterns("/static/layui/**");
@@ -52,6 +52,6 @@ public class InterceptorConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler(this.staticPathPattern).addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
     }
 }
